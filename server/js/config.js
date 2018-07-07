74719258af02a3a7bdb643b5da31bf085a6b4c45
Nicolas Maier <Nicolas.Maier@edu.hefr.ch>
2018-07-07 22:34:39 +0200

    Test

--- server/js/config.js
+++ server/js/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Port du serveur http (y compris le websocket)
-  httpServerPort: 80,
+  httpServerPort: 5000,
   // Dossier à servir pour le serveur web
   httpFolderToServe: '../client/build',
   // Fichier contenant les logs d'erreur du serveur
